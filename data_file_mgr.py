d55d121160a9ec9df1352b27cbe319f013fdd84b
dcook <daniel.j.cook.28@gmail.com>
2025-06-18 19:56:52 -0500

    Added streamlit chart that auto refreshes as temp values as scraped.  Also added an initialization step if file is blank

--- data_file_mgr.py
+++ data_file_mgr.py
@@ -7,9 +7,10 @@ def initiate_file():
     if(not content.startswith('dates,temperature')):
         with open("temp.txt", 'w') as file:
             file.write("dates,temperature\n")
+        return True
+    else:
+        return False
 
-def save_to_file(temp):
+def save_to_file(new_time, new_temp):
     with open("temp.txt", 'a') as file:
-        current_time = datetime.datetime.now()
-        current_time = datetime.datetime.strftime(current_time, "%y-%m-%d-%H-%M-%S")
-        file.write(f"{current_time},{temp}\n")
+        file.write(f"{new_time},{new_temp}\n")
--- main.py
+++ main.py
@@ -1,13 +1,33 @@
 import time
+import pandas as pd
 import scraper
 import data_file_mgr as dfm
 import streamlit as st
+import pandas
+import datetime
 
 URL = "https://programmer100.pythonanywhere.com/"
 
-if __name__ == "__main__":
-    dfm.initiate_file()
-    while True:
-        extracted_temp = scraper.extraction(URL)
-        dfm.save_to_file(extracted_temp)
-        time.sleep(2)
+def extract_and_save():
+    extracted_temp = float(scraper.extraction(URL))
+
+    current_time = datetime.datetime.now()
+    current_time = datetime.datetime.strftime(current_time, "%y-%m-%d-%H-%M-%S")
+
+    dfm.save_to_file(current_time, extracted_temp)
+    new_row = {"dates": current_time, "temperature": extracted_temp}
+    return new_row
+
+if(dfm.initiate_file()):
+    extract_and_save()
+
+df = pandas.read_csv("temp.txt")
+chart = st.line_chart(df, x="dates", y="temperature")
+
+while True:
+    new_row = extract_and_save()
+
+    df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+    chart.add_rows(pd.DataFrame([new_row]))
+
+    time.sleep(2)
